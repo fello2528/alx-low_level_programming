3c549ba9cac04765942b2885c7c5aa512406084f
fello2528 <felixmwangimukoma@gmail.com>
2022-09-09 02:37:20 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry
+ * main - Entry point
  *
- * return: always 0
+ * Return: always 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
