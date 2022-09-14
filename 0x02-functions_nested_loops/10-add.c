c7b69b3370aef01810e745a228e3964feb9e504f
fello2528 <felixmwangimukoma@gmail.com>
2022-09-14 10:22:41 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - addittion of int i, int j
+ * @i: first integer
+ * @j: second integer
+ * Return: return addittion of int i and int j
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
