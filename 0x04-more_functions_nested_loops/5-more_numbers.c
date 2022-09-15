b14805c9c4fedb2ba72c302cce63e865175e80cf
fello2528 <felixmwangimukoma@gmail.com>
2022-09-15 09:00:38 -0700

    more no

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		_putchar(i);
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+		_putchar(j / 10 + '0');
+			}
+		_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
