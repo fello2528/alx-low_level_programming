900630f45198cb51298e1ec3c6711960e1af74f4
fello2528 <felixmwangimukoma@gmail.com>
2022-09-26 11:50:46 -0700

    diagonal sum

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,8 @@
 #ifndef _PUTCHAR
 #define _PUTCHAR
+
+#include <stdio.h>
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
