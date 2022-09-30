1a396669a1194d443066c48543b90a53e66c36d3
fello2528 <felixmwangimukoma@gmail.com>
2022-09-30 07:15:10 -0700

    print all args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,13 +4,13 @@
 /**
  * main - main code
  * @argc: argument count
- * @argv[]: argument string
+ * @argv: argument vector
  * Return: always 0
  */
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
 	{
 	printf("%s\n", argv[i]);
