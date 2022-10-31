9e7fefe01db1765ef7362796220c6f425c7250c5
fello2528 <felixmwangimukoma@gmail.com>
2022-10-31 10:25:04 -0700

    binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,12 +1,8 @@
 #include "main.h"
 /**
- *
- *  * get_msb - gets the most significant bit of a number
- *
- *   * @n: the number
- *
- *     * Return: the msb
- *
+ * get_msb - gets the most significant bit of a number
+ * @n: the number
+ * Return: the msb
  */
 unsigned long int get_msb(unsigned long int n)
 {
@@ -22,16 +18,11 @@ unsigned long int get_msb(unsigned long int n)
 }
 
 /**
+ * print_binary - prints the binary representation of a number.
  *
- *  * print_binary - prints the binary representation of a number.
- *
- *   * @n: the number
- *
- *    *
- *
- *     * Return: void
+ * @n: the number
+ * Return: void
  */
-
 void print_binary(unsigned long int n)
 {
 		int hit_first_1 = 0;
@@ -39,20 +30,20 @@ void print_binary(unsigned long int n)
 		int i;
 		unsigned long int cur_bit;
 
-		if (n == 0)					
+		if (n == 0)
 		{
 			_putchar('0');
 			return;
 		}
-		
 		num_bits = sizeof(n) * 8;
 
 		for (i = 0; i < num_bits; i++)
 		{
 			cur_bit = get_msb(n);
-			if (cur_bit == 1)		
+			if (cur_bit == 1)
 				hit_first_1 = 1;
-			if (hit_first_1)																_putchar(cur_bit + '0');
+			if (hit_first_1)
+				_putchar(cur_bit + '0');
 			n <<= 1;
 		}
 }
